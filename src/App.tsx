085099d2b441b4a59848f3ab147222505cdab2a9
Topsyaka077 <topsyaka077@gmail.com>
2022-09-18 00:23:06 +0300

    Add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { GameField } from './components/GameField';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
+import { GameField } from './components/GameField'
 
 function App() {
   return (
     <div className="App">
-      <GameField xSize={100} ySize={100}></GameField>
+      <GameField xSize={100} ySize={100} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
