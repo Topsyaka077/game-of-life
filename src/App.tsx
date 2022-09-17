08fb641a066b0841eec39e8377ea713bea87742b
Topsyaka077 <topsyaka077@gmail.com>
2022-09-18 00:38:08 +0300

    Fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import React from 'react'
-import logo from './logo.svg'
-import './App.css'
 import { GameField } from './components/GameField'
 
 function App() {
   return (
     <div className="App">
-      <GameField xSize={100} ySize={100} />
+      <GameField xSize={10} ySize={10} />
     </div>
   )
 }
